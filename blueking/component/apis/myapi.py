2bddefeba592b29ae1beb285548313f75dd4eadc
lanhaibin <lanhaibin@shengheyouxi.com>
2019-06-14 17:39:58 +0800

    更正描述

--- blueking/component/apis/myapi.py
+++ blueking/component/apis/myapi.py
@@ -10,5 +10,5 @@ class CollectionsMyAPI(object):
         self.get_dfinfo = ComponentAPI(
             client=self.client, method='GET',
             path='/api/c/self-service-api/host/get_dfinfo_lanhaibin/',
-            description=u"内存使用率查询"
+            description=u"磁盘使用率查询"
         )
