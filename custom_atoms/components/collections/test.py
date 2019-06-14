043d9a2c0cca2901379234801ecc737ce9678495
lanhaibin <lanhaibin@shengheyouxi.com>
2019-06-14 17:41:58 +0800

    test

--- custom_atoms/components/collections/test.py
+++ custom_atoms/components/collections/test.py
@@ -25,7 +25,8 @@ class TestCustomService(Service):
         path_input = data.get_one_of_inputs('eb_path_input')
 
         api_kwargs = {
-            'bk_app_code': APP_ID,
+            # 'bk_app_code': APP_ID,
+            'bk_app_code': 'mydc',
             'bk_username': executor,
             'ip': ip_input,
             'system': system_radio,
