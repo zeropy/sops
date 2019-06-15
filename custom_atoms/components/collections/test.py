e4d957d3463c44f8dcfbe40f606c18a28c2e531f
legion <1931330336@qq.com>
2019-06-15 11:13:55 +0800

    test

--- custom_atoms/components/collections/test.py
+++ custom_atoms/components/collections/test.py
@@ -39,7 +39,7 @@ class TestCustomService(Service):
 
     def outputs_format(self):
         return [
-            self.OutpputItem(name="磁盘使用率".encode('utf-8'), key='data', type='int')
+            self.OutpputItem(name="磁盘使用率".encode('utf-8').decode('utf-8'), key='data', type='int')
             # self.OutputItem(name='disk_usaged', key='data', type='int')
         ]
 
